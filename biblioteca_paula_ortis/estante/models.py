29228b9b7883cb24e6da48bb9d87fa5822c1a3d3
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-06-03 18:29:44 -0300

    add titulo das colunas do sqlite

--- biblioteca_paula_ortis/estante/models.py
+++ biblioteca_paula_ortis/estante/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Book(models.Model):
+    numero_id = models.ForeignKey(int, unique=True, on_delete=models.CASCADE)
+    nome_livro = models.TextField()
+    autores = models.TextField()
+    situacao = models.CharField()
+    cadastrado_em = models.DateTimeField(auto_now_add=True)
+    ultima_alteracao = models.DateTimeField(auto_now=True)
+    responsavel_alteracao = models.CharField(User)
