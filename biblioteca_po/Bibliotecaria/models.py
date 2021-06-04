dfb468dbf44d82538704ba7c9b2f87fb1c0d0538
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-06-04 04:35:13 -0300

    starting new ideia

--- biblioteca_po/Bibliotecaria/models.py
+++ biblioteca_po/Bibliotecaria/models.py
@@ -2,3 +2,14 @@ from django.contrib.auth.models import AbstractUser
 
 class Bibliotecaria(AbstractUser):
     pass
+
+
+# class Books(models.Model):
+#     numero_id = models.DecimalField(primary_key=True , max_digits=4, unique=True, blank=False, null=False)
+#     nome_livro = models.TextField(blank=False, null=False)
+#     slug = models.SlugField(nome_livro)
+#     autores = models.TextField( default='Desconhecido', null=False)
+#     # situacao = models.CharField(max_length=10)
+#     cadastrado_em = models.DateTimeField(auto_now_add=True)
+#     ultima_alteracao = models.DateTimeField(auto_now=True)
+#     responsavel_alteracao = models.ForeignKey(User, on_delete=models.CASCADE)
--- biblioteca_po/estante/models.py
+++ biblioteca_po/estante/models.py
@@ -1,12 +1,4 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Books(models.Model):
-    numero_id = models.DecimalField(primary_key=True , max_digits=4, unique=True, blank=False, null=False)
-    nome_livro = models.TextField(blank=False, null=False)
-    slug = models.SlugField(nome_livro)
-    autores = models.TextField( default='Desconhecido', null=False)
-    # situacao = models.CharField(max_length=10)
-    cadastrado_em = models.DateTimeField(auto_now_add=True)
-    ultima_alteracao = models.DateTimeField(auto_now=True)
-    responsavel_alteracao = models.ForeignKey(User, on_delete=models.CASCADE)
+# hehe
