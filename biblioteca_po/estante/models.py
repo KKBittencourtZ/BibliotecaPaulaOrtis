ecffc49bd7dd4adf73da58aa8b9c947e26ddb248
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-06-04 01:25:18 -0300

    tentativa enésima

--- biblioteca_po/estante/models.py
+++ biblioteca_po/estante/models.py
@@ -1,12 +1,12 @@
-from django.contrib.auth.models import User
+from django.contrib import admin
 from django.db import models
 
 class Books(models.Model):
-    numero_id = models.ForeignKey(primary_key=True ,on_delete=models.CASCADE,max_length=4, unique=True, blank=False, null=False)
+    numero_id = models.DecimalField(primary_key=True , max_digits=4, unique=True, blank=False, null=False)
     nome_livro = models.TextField(blank=False, null=False)
     slug = models.SlugField(nome_livro)
-    autores = models.TextField(blank=False, null=False)
-    situacao = models.CharField(max_length=10)
+    autores = models.TextField( default='Desconhecido', null=False)
+    # situacao = models.CharField(max_length=10)
     cadastrado_em = models.DateTimeField(auto_now_add=True)
     ultima_alteracao = models.DateTimeField(auto_now=True)
-    responsavel_alteracao = models.CharField(User)
+    responsavel_alteracao = models.ForeignKey(admin, on_delete=models.CASCADE)
